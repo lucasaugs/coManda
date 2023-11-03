877a7a3ca2ec90910bdd9d70fc5410bafeef31e8
Caio Teles <caiotelescunha2001@gmail.com>
2023-11-03 13:19:13 -0300

    wip: broke bd

---
+++ backend-comanda-api/prisma/migrations/20231103161500_init/migration.sql
@@ -0,0 +1,82 @@
+-- CreateTable
+CREATE TABLE "restaurant" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "cpf" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "cep" TEXT NOT NULL,
+    "street" TEXT NOT NULL,
+    "neighborhood" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "sheetClient" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "total" INTEGER NOT NULL DEFAULT 0,
+    "isOpen" BOOLEAN NOT NULL DEFAULT true,
+    "restaurantId" INTEGER NOT NULL,
+    CONSTRAINT "sheetClient_restaurantId_fkey" FOREIGN KEY ("restaurantId") REFERENCES "restaurant" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "user" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "cpf" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "item" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "price" INTEGER NOT NULL,
+    "picture" TEXT NOT NULL,
+    "restaurantId" INTEGER NOT NULL,
+    CONSTRAINT "item_restaurantId_fkey" FOREIGN KEY ("restaurantId") REFERENCES "restaurant" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "_sheetClientTouser" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL,
+    CONSTRAINT "_sheetClientTouser_A_fkey" FOREIGN KEY ("A") REFERENCES "sheetClient" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "_sheetClientTouser_B_fkey" FOREIGN KEY ("B") REFERENCES "user" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "_itemTouser" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL,
+    CONSTRAINT "_itemTouser_A_fkey" FOREIGN KEY ("A") REFERENCES "item" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "_itemTouser_B_fkey" FOREIGN KEY ("B") REFERENCES "user" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "restaurant_email_key" ON "restaurant"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "restaurant_cpf_key" ON "restaurant"("cpf");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "sheetClient_restaurantId_key" ON "sheetClient"("restaurantId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_email_key" ON "user"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_cpf_key" ON "user"("cpf");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_sheetClientTouser_AB_unique" ON "_sheetClientTouser"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_sheetClientTouser_B_index" ON "_sheetClientTouser"("B");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_itemTouser_AB_unique" ON "_itemTouser"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_itemTouser_B_index" ON "_itemTouser"("B");
--- backend-comanda-api/prisma/seed.ts
+++ backend-comanda-api/prisma/seed.ts
@@ -1,49 +1,49 @@
 import { db } from "../src/utils/db.server";
 
-type users = {
-    name: string;
-    email: string;
-    password: string;
-    cpf: string;
-};
+// type users = {
+//     name: string;
+//     email: string;
+//     password: string;
+//     cpf: string;
+// };
 
-async function seed() {
-    await Promise.all(
-        getUsers().map((user) => {
-            return db.users.create({
-                data: {
-                    name: user.name,
-                    email: user.email,
-                    password: user.password,
-                    cpf: user.cpf,
-                }
-            });
-        })
-    );
-}
+// async function seed() {
+//     await Promise.all(
+//         getUsers().map((user) => {
+//             return db.users.create({
+//                 data: {
+//                     name: user.name,
+//                     email: user.email,
+//                     password: user.password,
+//                     cpf: user.cpf,
+//                 }
+//             });
+//         })
+//     );
+// }
 
-seed();
+// seed();
 
-function getUsers(): Array<users>{
-    return [
-        {
-            name: "caio",
-            email: "caioteles@gmail.com",
-            password: "123456",
-            cpf: "12345678910",
-        },
-        {
-            name: "alvaro",
-            email: "alvaroteles@gmail.com",
-            password: "123456",
-            cpf: "22345678910",
-        },
-        {
-            name: "lorena",
-            email: "lorenateles@gmail.com",
-            password: "123456",
-            cpf: "32345678910",
-        },
-    ];
-}
+// function getUsers(): Array<users>{
+//     return [
+//         {
+//             name: "caio",
+//             email: "caioteles@gmail.com",
+//             password: "123456",
+//             cpf: "12345678910",
+//         },
+//         {
+//             name: "alvaro",
+//             email: "alvaroteles@gmail.com",
+//             password: "123456",
+//             cpf: "22345678910",
+//         },
+//         {
+//             name: "lorena",
+//             email: "lorenateles@gmail.com",
+//             password: "123456",
+//             cpf: "32345678910",
+//         },
+//     ];
+// }
 
