4b311d955254d81532497fd370683843b48d34bc
Lucas Gonçalves <lucasaugusto83.la@gmail.com>
2023-11-05 23:04:20 -0300

    Rotas de cadastro finalizadas

--- coManda-vue/src/views/ClientSingUpPage.vue
+++ coManda-vue/src/views/ClientSingUpPage.vue
@@ -1,6 +1,39 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import BoxInput from "../components/BoxInput.vue"
 import Btnlogin from "../components/BtnLogin.vue"
+import axios from "axios";
+import { AppStore } from "../common/AppStore.js";
+import { router } from "../router/index.js";
+
+const registerUser = ref({
+  email: "",
+  name: "",
+  cpf: "",
+  password: "",
+  confirmPassword: "",
+})
+
+const signupUser = () => {
+  if (registerUser.value.password != registerUser.value.confirmPassword) {
+    alert("As senhas não coincidem")
+    return
+  }
+  axios.post("http://localhost:3030/api/users/client-signUp",
+    {
+      email: registerUser.value.email,
+      name: registerUser.value.name,
+      cpf: registerUser.value.cpf,
+      password: registerUser.value.password,
+    })
+    .then((res) => {
+      alert("Usuário cadastrado com sucesso!")
+      router.push("/login")
+    })
+    .catch((err) => {
+      alert("Erro ao cadastrar usuário")
+    })
+}
 </script>
     
 <template>
@@ -13,17 +46,17 @@ import Btnlogin from "../components/BtnLogin.vue"
     <p style="font-size: 34px; color: #e0e1dd;">CADASTRE-SE</p>
     <div class="w-50 mx-auto">
       <BoxInput :placeholder="'Email'" :customClass="'my-4 w-75 mx-auto'" :customStyle="'height: 80px;font-size:24px'"
-        type="email" />
+        type="email" v-model:value="registerUser.email" />
       <BoxInput :placeholder="'Nome Completo'" :customClass="'my-4 w-75 mx-auto'"
-        :customStyle="'height: 80px;font-size:24px'" type="text" />
+        :customStyle="'height: 80px;font-size:24px'" type="text" v-model:value="registerUser.name" />
       <BoxInput :placeholder="'CPF'" :customClass="'my-4 w-75 mx-auto'" :customStyle="'height: 80px;font-size:24px'"
-        type="text" pattern="\d{11}" />
+        type="text" pattern="\d{11}" v-model:value="registerUser.cpf" />
       <BoxInput :placeholder="'Senha'" :customClass="'my-4 w-75 mx-auto'" :customStyle="'height: 80px;font-size:24px'"
-        type="password" />
+        type="password" v-model:value="registerUser.password" />
       <BoxInput :placeholder="'Confirmar senha'" :customClass="'my-4 w-75 mx-auto'"
-        :customStyle="'height: 80px;font-size:24px'" type="password" />
+        :customStyle="'height: 80px;font-size:24px'" type="password" v-model:value="registerUser.confirmPassword" />
       <Btnlogin :msg="'Cadastrar'" :customClass="'mb-4 w-75 mx-auto'" :customStyle="'height: 80px; font-size:36px'"
-        type="submit" />
+        type="submit" @click="signupUser" />
     </div>
   </div>
 </template>
--- coManda-vue/src/views/RestaurantSingUpPage.vue
+++ coManda-vue/src/views/RestaurantSingUpPage.vue
@@ -1,6 +1,43 @@
 <script setup lang="ts">
 import BoxInput from "../components/BoxInput.vue"
 import Btnlogin from "../components/BtnLogin.vue"
+import axios from "axios";
+import { router } from "../router/index.js";
+import { ref } from "vue"
+
+const registerRestaurant = ref({
+  email: "",
+  cnpj: "",
+  name: "",
+  cep: "",
+  street: "",
+  neighborhood: "",
+  password: "",
+  confirmPassword: "",
+})
+
+const signupRestaurant = () => {
+  if (registerRestaurant.value.password != registerRestaurant.value.confirmPassword) {
+    alert("As senhas não coincidem")
+    return
+  }
+
+  axios.post("http://localhost:3030/api/restaurants/restaurant-signUp",
+    {
+      email: registerRestaurant.value.email,
+      cpf: registerRestaurant.value.cnpj,
+      name: registerRestaurant.value.name,
+      cep: registerRestaurant.value.cep,
+      street: registerRestaurant.value.street,
+      neighborhood: registerRestaurant.value.neighborhood,
+      password: registerRestaurant.value.password,
+    })
+    .then((res) => {
+      alert("Estabelecimento cadastrado com sucesso!")
+    }).catch((err) => {
+      alert("Erro ao cadastrar estabelecimento")
+    })
+}
 </script>
     
 <template>
@@ -16,33 +53,41 @@ import Btnlogin from "../components/BtnLogin.vue"
     <div class="w-20 mx-auto">
       <div class="row justify-content-center">
         <div class="my-2 p-2 mx-4 col-5">
-          <BoxInput :placeholder="'Email'" :customStyle="'height: 80px;font-size:24px'" type="email" />
+          <BoxInput :placeholder="'Email'" :customStyle="'height: 80px;font-size:24px'" type="email"
+            v-model:value="registerRestaurant.email" />
         </div>
         <div class="my-2 p-2 mx-4 col-5">
-          <BoxInput :placeholder="'CNPJ'" :customStyle="'height: 80px;font-size:24px'" type="text" pattern="\d{14}" />
+          <BoxInput :placeholder="'CNPJ'" :customStyle="'height: 80px;font-size:24px'" type="text" pattern="\d{14}"
+            v-model:value="registerRestaurant.cnpj" />
         </div>
         <div class="my-2 p-2 mx-4 col-5">
-          <BoxInput :placeholder="'Nome completo'" :customStyle="'height: 80px;font-size:24px'" type="text" />
+          <BoxInput :placeholder="'Nome completo'" :customStyle="'height: 80px;font-size:24px'" type="text"
+            v-model:value="registerRestaurant.name" />
         </div>
         <div class="my-2 p-2 mx-4 col-5">
-          <BoxInput :placeholder="'CEP'" :customStyle="'height: 80px;font-size:24px'" type="text" pattern="\d{8}" />
+          <BoxInput :placeholder="'CEP'" :customStyle="'height: 80px;font-size:24px'" type="text" pattern="\d{8}"
+            v-model:value="registerRestaurant.cep" />
         </div>
         <div class="my-2 p-2 mx-4 col-5">
-          <BoxInput :placeholder="'Rua do estabelecimento'" :customStyle="'height: 80px;font-size:24px'" type="text" />
+          <BoxInput :placeholder="'Rua do estabelecimento'" :customStyle="'height: 80px;font-size:24px'" type="text"
+            v-model:value="registerRestaurant.street" />
         </div>
         <div class="my-2 p-2 mx-4 col-5">
-          <BoxInput :placeholder="'Bairro do estabelecimento'" :customStyle="'height: 80px;font-size:24px'" type="text" />
+          <BoxInput :placeholder="'Bairro do estabelecimento'" :customStyle="'height: 80px;font-size:24px'" type="text"
+            v-model:value="registerRestaurant.neighborhood" />
         </div>
         <div class="my-2 p-2 mx-4 col-5">
-          <BoxInput :placeholder="'Senha'" :customStyle="'height: 80px;font-size:24px'" type="password" />
+          <BoxInput :placeholder="'Senha'" :customStyle="'height: 80px;font-size:24px'" type="password"
+            v-model:value="registerRestaurant.password" />
         </div>
         <div class="my-2 p-2 mx-4 col-5">
-          <BoxInput :placeholder="'Confirmar senha'" :customStyle="'height: 80px;font-size:24px'" type="password" />
+          <BoxInput :placeholder="'Confirmar senha'" :customStyle="'height: 80px;font-size:24px'" type="password"
+            v-model:value="registerRestaurant.confirmPassword" />
         </div>
       </div>
       <router-link to="/restaurant-signUp/menu">
         <Btnlogin :msg="'Próximo'" :customClass="'mb-4 w-50 mx-auto'" :customStyle="'height: 80px; font-size:36px'"
-          type="submit" />
+          type="submit" @click="signupRestaurant" />
       </router-link>
     </div>
   </div>
